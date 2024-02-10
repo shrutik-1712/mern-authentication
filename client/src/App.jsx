5888e726f4e1325f539f6cb8e0c03d6547de73e2
Shrutik Dawane <shrutikdawane@gmail.com>
2024-02-10 11:51:10 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+  
+  return (
+    <>
+      <div className='text-red-950'><h2 className='text-red-500'>hello</h2>  </div>
+    </>
+  )
+}
+
+export default App
